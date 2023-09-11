e5b2d2e55d87be85104b9e3d840cd819f1220e8a
WillOli <william.ika@gmail.com>
2023-09-11 11:24:13 -0300

    Ordenacao Set

---
+++ Ordenacao-Produto/src/CadastroProdutos.java
@@ -0,0 +1,43 @@
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class CadastroProdutos {
+
+    Set<Produto> produtoSet;
+
+    public CadastroProdutos() {
+        this.produtoSet = new HashSet<>();
+    }
+
+    public void adicionarProduto(long codigo, String nome, double preco, int quantidade) {
+        produtoSet.add(new Produto(codigo,nome, preco, quantidade));
+    }
+
+    public Set<Produto> exibirProdutosPorNome() {
+        Set<Produto> produtosPorNome = new TreeSet<>(produtoSet);
+        if (!produtoSet.isEmpty()) {
+            return produtosPorNome;
+        } else {
+            throw new RuntimeException("O conjunto está vazio!");
+        }
+    }
+    public Set<Produto> exibirProdutosPorPreco() {
+        Set<Produto> produtoPorPreco = new TreeSet<>(new CompratorPorPreco());
+        if (!produtoSet.isEmpty()) {
+            produtoPorPreco.addAll(produtoSet);
+            return produtoPorPreco;
+        } else {
+            throw new RuntimeException("O conjunto está vazio!");
+        }
+    }
+
+    class CompratorPorPreco implements Comparator<Produto> {
+        @Override
+        public int compare(Produto p1, Produto p2) {
+            return Double.compare(p1.getPreco(), p2.getPreco());
+        }
+
+    }
+}
