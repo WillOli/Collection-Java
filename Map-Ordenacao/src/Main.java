4e7b05299ecb29ad885e7b8bd361d073c94874e2
WillOli <william.ika@gmail.com>
2023-09-12 23:04:18 -0300

    Pesquisa ordenacao> testando os metodos

--- Map-Ordenacao/src/Main.java
+++ Map-Ordenacao/src/Main.java
@@ -1,6 +1,20 @@
+import java.time.LocalDate;
+import java.time.Month;
+
 public class Main {
     public static void main(String[] args) {
         AgendaEventos agendaEventos = new AgendaEventos();
+
+        agendaEventos.adicionarEvento(LocalDate.of(2023, Month.SEPTEMBER, 12), "Evento 1", "Atração 1");
+        agendaEventos.adicionarEvento(LocalDate.of(2021, Month.SEPTEMBER, 12), "Evento 2", "Atração 2");
+        agendaEventos.adicionarEvento(LocalDate.of(2020, Month.JULY, 14), "Evento 3", "Atração 3");
+        agendaEventos.adicionarEvento(LocalDate.of(2024, Month.SEPTEMBER, 12), "Evento 4", "Atração 4");
+
+
+        agendaEventos.exibirAgenda();
+
+        agendaEventos.obterProximoEvento();
+
 
     }
 }
