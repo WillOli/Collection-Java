808bb978f49cfe07d40e12a1432b22562001b1ef
WillOli <william.ika@gmail.com>
2023-09-12 22:55:15 -0300

    Ordenacao Map - testando os metodos

--- Map-Ordenacao/src/AgendaEventos.java
+++ Map-Ordenacao/src/AgendaEventos.java
@@ -11,12 +11,28 @@ public class AgendaEventos {
     }
 
     public void adicionarEvento(LocalDate data, String nome, String atracao) {
-        Evento evento = new Evento(nome, atracao);
-        eventosMap.put(data, evento);
+        // Evento evento = new Evento(nome, atracao);
+        eventosMap.put(data, new Evento(nome, atracao));
     }
 
     public void exibirAgenda() {
         Map<LocalDate, Evento> eventoMapTreeMap = new TreeMap<>(eventosMap);
         System.out.println(eventoMapTreeMap);
     }
+
+    public void obterProximoEvento() {
+        LocalDate dataatual = LocalDate.now();
+        LocalDate proximaData = null;
+        Evento proximoEvento = null;
+        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventosMap);
+        for (Map.Entry<LocalDate, Evento> entry : eventosMap.entrySet()) {
+            if(entry.getKey().isEqual(dataatual) || entry.getKey().isAfter(dataatual)) {
+                proximaData = entry.getKey();
+                proximoEvento = entry.getValue();
+                System.out.println("O próximo evento: " + proximoEvento + " acontecerá na data " + proximaData);
+                break;
+            }
+        }
+    }
+
 }
